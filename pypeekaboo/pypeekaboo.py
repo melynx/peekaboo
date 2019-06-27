dab305f15ccca5e8e61700b4347330cefe0cf3ff
Chua Zheng Leong <melynx@gmail.com>
2019-06-27 18:50:23 +0800

    Add get_insn(insn_id)

--- pypeekaboo/pypeekaboo.py
+++ pypeekaboo/pypeekaboo.py
@@ -2,7 +2,80 @@ import os
 import sys
 import struct
 
-from ctypes import Structure, c_uint64, c_uint32, c_uint8, sizeof
+from ctypes import Structure, c_uint64, c_uint32, c_uint16, c_uint8, sizeof
+
+def read_struct(myfile, mystruct):
+    x = mystruct()
+    assert(myfile.readinto(x) == sizeof(mystruct))
+    return x
+
+
+class GPR_AMD64(Structure):
+    _fields_ = [('rdi', c_uint64),
+                ('rsi', c_uint64),
+                ('rsp', c_uint64),
+                ('rbp', c_uint64),
+                ('rbx', c_uint64),
+                ('rdx', c_uint64),
+                ('rcx', c_uint64),
+                ('rax', c_uint64),
+                ('r8', c_uint64),
+                ('r9', c_uint64),
+                ('r10', c_uint64),
+                ('r11', c_uint64),
+                ('r12', c_uint64),
+                ('r13', c_uint64),
+                ('r14', c_uint64),
+                ('r15', c_uint64),
+                ('rflags', c_uint64),
+                ('rip', c_uint64)]
+
+class SIMD_AMD64(Structure):
+    _fields_ = [('ymm0', c_uint64*4),
+                ('ymm1', c_uint64*4),
+                ('ymm2', c_uint64*4),
+                ('ymm3', c_uint64*4),
+                ('ymm4', c_uint64*4),
+                ('ymm5', c_uint64*4),
+                ('ymm6', c_uint64*4),
+                ('ymm7', c_uint64*4),
+                ('ymm8', c_uint64*4),
+                ('ymm9', c_uint64*4),
+                ('ymm10', c_uint64*4),
+                ('ymm11', c_uint64*4),
+                ('ymm12', c_uint64*4),
+                ('ymm13', c_uint64*4),
+                ('ymm14', c_uint64*4),
+                ('ymm15', c_uint64*4)]
+
+class FXSAVE_AREA(Structure):
+    _fields_ = [('fcw', c_uint16),
+                ('fsw', c_uint16),
+                ('ftw', c_uint8),
+                ('reserved_1', c_uint8),
+                ('fop', c_uint16),
+                ('fpu_ip', c_uint32),
+                ('fpu_cs', c_uint16),
+                ('reserved_2', c_uint16),
+                ('fpu_dp', c_uint32),
+                ('fpu_ds', c_uint16),
+                ('reserved_3', c_uint16),
+                ('mxcsr', c_uint32),
+                ('mxcsr_mask', c_uint32),
+                ('st_mm', c_uint64*2*8),
+                ('xmm', c_uint64*2*16),
+                ('padding', c_uint8*96)]
+
+
+class RegFileAMD64(Structure):
+    _fields_ = [('gpr', GPR_AMD64), ('simd', SIMD_AMD64), ('fxsave', FXSAVE_AREA)]
+
+
+
+ARCH_REGFILE = {0:None, 1:None, 2:None, 3:RegFileAMD64}
+
+class Metadata(Structure):
+    _fields_ = [('arch', c_uint32), ('version', c_uint32)]
 
 '''
 typedef struct insn_ref {
@@ -44,6 +117,11 @@ class MemFile(Structure):
 
 class TraceInsn(object):
     def __init__(self):
+        self.addr = None
+        self.rawbytes = None
+        self.num_mem = None
+        self.mem = []
+        self.regfile = None
         pass
 
 class MemInfo(object):
@@ -76,6 +154,10 @@ class PyPeekaboo(object):
         self.memrefs = open(memrefs_path, 'rb')
         self.metafile = open(metafile_path, 'rb')
 
+        # parse metafile
+        metadata = read_struct(self.metafile, Metadata)
+        self.regfile_struct = ARCH_REGFILE[metadata.arch]
+
         self.memrefs_offsets = self.load_memrefs_offsets(trace_path)
         self.num_insn = os.path.getsize(insn_trace_path) / sizeof(InsnRef)
 
@@ -104,31 +186,33 @@ class PyPeekaboo(object):
         return open(memrefs_offsets_path, 'rb')
 
     def get_insn(self, insn_id):
+
+        # get the offset of the instruction into the different files.
+        insn_trace_foffset = insn_id * sizeof(InsnRef)
+        memrefs_foffset = insn_id * sizeof(MemRef)
+        regfile_foffset = insn_id * sizeof(self.regfile_struct)
+        memfile_index_foffset = insn_id * 8
+
         my_insn = TraceInsn()
 
-        x = InsnRef()
-        self.insn_trace.seek(insn_id * sizeof(InsnRef))
-        assert(self.insn_trace.readinto(x) == sizeof(InsnRef))
-        my_insn.addr = x.pc
-        my_insn.rawbytes = self.bytesmap[x.pc]
+        self.insn_trace.seek(insn_trace_foffset)
+        my_insn.addr = read_struct(self.insn_trace, InsnRef).pc
+        my_insn.rawbytes = self.bytesmap[my_insn.addr]
 
-        x = MemRef()
-        self.memrefs.seek(insn_id * sizeof(MemRef))
-        assert(self.memrefs.readinto(x) == sizeof(MemRef))
-        my_insn.num_mem = x.length
-
+        self.memrefs.seek(memrefs_foffset)
+        my_insn.num_mem = read_struct(self.memrefs, MemRef).length
 
         my_insn.mem = []
         if my_insn.num_mem:
-            self.memrefs_offsets.seek(insn_id * 8)
+            self.memrefs_offsets.seek(memfile_index_foffset)
             for _ in range(my_insn.num_mem):
                 buf = self.memrefs_offsets.read(8)
                 memref_offset = struct.unpack('<Q', buf)[0]
-                memfile = MemFile()
                 self.memfile.seek(memref_offset)
-                assert(self.memfile.readinto(memfile)==sizeof(MemFile))
-                print(memfile)
+                my_insn.mem.append(read_struct(self.memfile, MemFile))
 
+        self.regfile.seek(regfile_foffset)
+        my_insn.regfile = read_struct(self.regfile, self.regfile_struct)
         return my_insn
 
     
