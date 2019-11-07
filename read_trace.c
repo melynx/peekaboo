9c92c5719f186fe86b86ff4601df36e3ca0df91e
Chua Zheng Leong <melynx@gmail.com>
2019-11-07 21:45:59 +0800

    Update new way of reading trace.

--- read_trace.c
+++ read_trace.c
@@ -1,62 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <inttypes.h>
 
 #include "libpeekaboo.h"
-#include "arch/amd64.h"
-
-typedef regfile_amd64_t regfile_ref_t;
 
 int main(int argc, char *argv[])
 {
-
-	insn_ref_t *insn_ref_buf = malloc(MEM_BUF_SIZE);
-	//regfile_ref_t *regfile_ref_buf = malloc(REG_BUF_SIZE);
-	regfile_ref_t *regfile_ref_buf = malloc(sizeof(regfile_ref_t)*MAX_NUM_REG_REFS);
-	bytes_map_t *bytes_map_buf;
-
 	char *trace_path = argv[1];
-
 	peekaboo_trace_t mytrace;
-	load_trace(sys.argv[1], &mytrace);
-	load_bytes_map(mytrace, bytes_map_buf);
+	load_trace(trace_path, &mytrace);
+	
+	size_t num_insn = get_num_insn(&mytrace);
 
-	size_t num_insn = num_insn(mytrace);
-	printf("Found %lu instructions in trace...\n", num_insn);
-
-	printf("Opening regfile file : %s\n", regfile_path);
-	printf("Found %lu instructions in regfile...\n", num_regfile);
-
-	if (num_regfile != num_insn)
+	for (int x=1; x<=num_insn; x++)
 	{
-		printf("num_regfile != num_insn!\n");
-		exit(1);
-	}
+		peekaboo_insn_t *insn = get_peekaboo_insn(x, &mytrace);
 
-	int valid_reads = 0;
-	int valid_reads2 = 0;
-	bytes_map_t *cur_bytes_map = NULL;
-	do {
-		valid_reads = fread(insn_ref_buf, sizeof(insn_ref_t), MAX_NUM_INS_REFS, insn_trace);
-		valid_reads2 = fread(regfile_ref_buf, sizeof(regfile_ref_t), MAX_NUM_REG_REFS, regfile);
-		if (valid_reads != valid_reads2)
-		{
-			printf("insn_ref_read != regfile_ref_read!\n");
-			exit(1);
-		}
-		for (int x=0; x<valid_reads; x++)
-		{
-			cur_bytes_map = find_bytes_map(insn_ref_buf[x].pc, bytes_map_buf, num_maps);
-			printf("0x%" PRIx64 "", insn_ref_buf[x].pc);
-			printf("\t size: %d", cur_bytes_map->size);
-			printf("\t rawbytes: ");
-			for (int y=0; y<cur_bytes_map->size; y++)
-				printf("%" PRIx8 " ", cur_bytes_map->rawbytes[y]);
-			regfile_pp(regfile_ref_buf[x]);
-			printf("\n");
-		}
-	} while (!feof(insn_trace));
+		printf("0x%" PRIx64 "", insn->addr);
+		printf("\t size: %d", insn->size);
+		printf("\t rawbytes: ");
+		for (int y=0; y<insn->size; y++)
+			printf("%" PRIx8 " ", insn->rawbytes[y]);
+		printf("\n");
+		regfile_pp(insn);
+		printf("\n");
+	}
 
 	return 0;
 }
