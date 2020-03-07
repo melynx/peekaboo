6acf4693cd3a8efeb997eb05c97f0e1a4e392504
Kaihang JI <jikaihang@pku.edu.cn>
2020-03-07 14:17:24 +0800

    Add headers required

--- peekaboo_dr/syscall.h
+++ peekaboo_dr/syscall.h
@@ -5,6 +5,17 @@
 #ifndef PEEKABOO_SYSCALL_H
 #define PEEKABOO_SYSCALL_H
 
+#ifdef UNIX
+#    ifdef LINUX
+#        include <syscall.h>
+#        define SYSNUM_SIGPROCMASK SYS_rt_sigprocmask
+#    else
+#        include <sys/syscall.h>
+#        define SYSNUM_SIGPROCMASK SYS_sigprocmask
+#    endif
+#    include <errno.h>
+#endif
+
 const char* get_syscall_name(int sysnum);
 
 #endif //PEEKABOO_SYSCALL_H
